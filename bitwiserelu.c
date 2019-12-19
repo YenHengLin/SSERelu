022b0b3861cfb836d500062eaccc430363453ee8
henry0929016816 <f74032112@mail.ncku.edu.tw>
2019-12-20 00:28:28 +0800

    Count 100000000 epoches time

--- bitwiserelu.c
+++ bitwiserelu.c
@@ -1,13 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <time.h>
+double tvgetf()
+{
+    struct timespec ts;
+    double sec;
+
+    clock_gettime(CLOCK_REALTIME, &ts);
+    sec = ts.tv_nsec;
+    sec /= 1e9;
+    sec += ts.tv_sec;
+
+    return sec;
+}
 int main()
 {
+    double t1, t2;
     int mask;
     int a[] = {-2, 0, 1, 2};
-    for (int i = 0; i < 4; i++) {
-        mask = a[i] >> 31;
-        a[i] = a[i] & ~(mask);
+    t1 = tvgetf();
+    for (int j = 0; j < 100000000; j++)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            mask = a[i] >> 31;
+            a[i] = a[i] & ~(mask);
+        }
     }
-    for (int i = 0; i < 4; i++) {
+    t2 = tvgetf();
+
+    for (int i = 0; i < 4; i++)
+    {
         printf("%d\n", a[i]);
     }
+    printf("%lf\n", t2 - t1);
 }
--- sserelu.c
+++ sserelu.c
@@ -1,13 +1,32 @@
 #include <emmintrin.h>
 #include <stdio.h>
+#include <time.h>
+double tvgetf()
+{
+    struct timespec ts;
+    double sec;
+
+    clock_gettime(CLOCK_REALTIME, &ts);
+    sec = ts.tv_nsec;
+    sec /= 1e9;
+    sec += ts.tv_sec;
+
+    return sec;
+}
 void main()
 {
+    double t1,t2;
     int a[] = {-2, 0, 1, 2};
-    __m128i inA = _mm_loadu_si128 ((__m128i const *)a);
-    __m128i mask = _mm_srai_epi32 (inA, 31);
-    inA = _mm_andnot_si128 (mask, inA);
-    _mm_storeu_si128 ((__m128i *) a, inA);
+    t1 = tvgetf();
+    for (int j = 0; j < 100000000; j++) {
+        __m128i inA = _mm_loadu_si128((__m128i const *)a);
+        __m128i mask = _mm_srai_epi32(inA, 31);
+        inA = _mm_andnot_si128(mask, inA);
+        _mm_storeu_si128((__m128i *)a, inA);
+    }
+    t2 = tvgetf();
     for (int i = 0; i < 4; i++) {
         printf("%d\n", a[i]);
     }
+    printf("%lf",t2-t1);
 }
